8a43613b65f6e5167b8c9f579a5ef85fddf4cb31
Luis Zavala <lz24069@gmail.com>
2020-05-23 15:33:57 -0400

    Updates

---
+++ precache-manifest.0870c6dc0d5048d3232b56627c9ae047.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b2585a00cc4898ddddf4b204e71b5f2",
+    "url": "/relativepath/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/relativepath/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2b1fbf04352050532557",
+    "url": "/relativepath/static/css/main.435d3cf4.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/relativepath/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/relativepath/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b1fbf04352050532557",
+    "url": "/relativepath/static/js/main.aa53c862.chunk.js"
+  },
+  {
+    "revision": "13e2932e12c6282734a2",
+    "url": "/relativepath/static/js/runtime-main.1902322f.js"
+  }
+]);
