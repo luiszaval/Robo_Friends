f68b7ba85403ac8e145ff1ac14e11ee7a00016d0
Luis Zavala <lz24069@gmail.com>
2020-05-23 15:44:07 -0400

    Updates

---
+++ precache-manifest.f16da3fdd56828f294d2bf68a44894c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47f13d4627134a9a17f4fa8ee8a4c2b2",
+    "url": "/Robo_Friends/index.html"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/Robo_Friends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "7e1b9fa79cb97ee8c113",
+    "url": "/Robo_Friends/static/css/main.435d3cf4.chunk.css"
+  },
+  {
+    "revision": "3dbaac5a522c343a46eb",
+    "url": "/Robo_Friends/static/js/2.d82cd422.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Robo_Friends/static/js/2.d82cd422.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e1b9fa79cb97ee8c113",
+    "url": "/Robo_Friends/static/js/main.68ca4917.chunk.js"
+  },
+  {
+    "revision": "02e26747c07b14dd3282",
+    "url": "/Robo_Friends/static/js/runtime-main.c7c3d9a1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/relativepath/precache-manifest.0870c6dc0d5048d3232b56627c9ae047.js"
+  "/Robo_Friends/precache-manifest.f16da3fdd56828f294d2bf68a44894c7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/relativepath/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Robo_Friends/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
